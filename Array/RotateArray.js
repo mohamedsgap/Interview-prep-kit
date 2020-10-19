1fc39bf47ee857e31beaa52ad86a8b2d05806a01
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-10-19 15:02:47 +0200

    Solve RotateArray

---
+++ Array/RotateArray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  const len = nums.length;
+  const diff = len - k;
+  const splicedList = [...nums].splice(0, k);
+  const restList = [...nums].slice(diff);
+  return [...restList, ...splicedList];
+};
+rotate([1, 5, 9, 6, 7, 4, 56, 5], 4);
